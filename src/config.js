8164dd628a6a8497807129dd86f2f706d27a627c
Admir Alicic <admir.alicic@forient.com>
2019-11-19 07:35:05 +0100

    Set up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'eu-central-1',
+    BUCKET: 'admir-alicic-notes-app-uploads'
+  },
+  apiGateway: {
+    REGION: 'eu-central-1',
+    URL: 'https://hvbyx5hghj.execute-api.eu-central-1.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'eu-central-1',
+    USER_POOL_ID: 'eu-central-1_ulaOZyHlW',
+    APP_CLIENT_ID: '46lkg0b97q57340bfe5m3u3gm8',
+    IDENTITY_POOL_ID: 'eu-central-1:46698070-103e-4bed-98b4-3190f8d241ce'
+  }
+};
